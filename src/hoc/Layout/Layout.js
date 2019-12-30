b8227109d1d4a127d93ea047b0ffc0019f302ac2
Dan Levy <danlevy124@gmail.com>
2019-12-29 19:58:24 -0500

    Up to, and including, lecture 364.

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import Aux from "../Aux/Aux";
 import Toolbar from "../../components/Navigation/Toolbar/Toolbar";
 import SideDrawer from "../../components/Navigation/SideDrawer/SideDrawer";
+import { connect } from "react-redux";
 import classes from "./Layout.module.css";
 
 class Layout extends Component {
@@ -22,10 +23,14 @@ class Layout extends Component {
     render() {
         return (
             <Aux>
-                <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler}/>
+                <Toolbar
+                    drawerToggleClicked={this.sideDrawerToggleHandler}
+                    isAuth={this.props.isAuthenticated}
+                />
                 <SideDrawer
                     open={this.state.showSideDrawer}
                     closed={this.sideDrawerClosedHandler}
+                    isAuth={this.props.isAuthenticated}
                 />
                 <main className={classes.Content}>{this.props.children}</main>
             </Aux>
@@ -33,4 +38,10 @@ class Layout extends Component {
     }
 }
 
-export default Layout;
+const mapStateToProps = state => {
+    return {
+        isAuthenticated: state.auth.token !== null
+    };
+};
+
+export default connect(mapStateToProps)(Layout);
---
+++ src/store/actions/auth.js
@@ -0,0 +1,64 @@
+import * as actionTypes from "./actionTypes";
+import axios from "axios";
+
+export const authStart = () => {
+    return {
+        type: actionTypes.AUTH_START
+    };
+};
+
+export const authSuccess = (token, userId) => {
+    return {
+        type: actionTypes.AUTH_SUCCESS,
+        idToken: token,
+        userId: userId
+    };
+};
+
+export const authFail = error => {
+    return {
+        type: actionTypes.AUTH_FAIL,
+        error: error
+    };
+};
+
+export const logout = () => {
+    return {
+        type: actionTypes.AUTH_LOGOUT
+    }
+}
+
+export const checkAuthTimeout = expirationTime => {
+    return dispatch => {
+        setTimeout(() => {
+            dispatch(logout());
+        }, expirationTime * 1000);
+    }
+}
+
+export const auth = (email, password, isSignup) => {
+    return dispatch => {
+        dispatch(authStart());
+        const authData = {
+            email: email,
+            password: password,
+            returnSecureToken: true
+        };
+        let url =
+            "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAFvRtJhg_rDezB3g2zD0ANFSCKkC6JsEw";
+        if (!isSignup) {
+            url =
+                "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAFvRtJhg_rDezB3g2zD0ANFSCKkC6JsEw";
+        }
+        axios
+            .post(url, authData)
+            .then(response => {
+                dispatch(authSuccess(response.data.idToken, response.data.localId));
+                dispatch(checkAuthTimeout(response.data.expiresIn))
+            })
+            .catch(error => {
+                console.log(error.response);
+                dispatch(authFail(error.response.data.error));
+            });
+    };
+};
